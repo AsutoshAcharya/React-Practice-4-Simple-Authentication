1f312eb07edc9beca8d6b8ef4827055f10d8eea2
Asutosh Acharya <asutosh246@gmail.com>
2022-11-22 18:56:53 +0530

    using context data to conditionally render lists

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,13 @@
-import { useState, useRef } from "react";
+import { useState, useRef,useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+
+  const authCtx=useContext(AuthContext);
 
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
@@ -56,6 +59,7 @@ const AuthForm = () => {
       })
       .then((data) => {
         console.log(data);
+        authCtx.login(data.idToken);
       })
       .catch((err) => {
         alert(err.message);
